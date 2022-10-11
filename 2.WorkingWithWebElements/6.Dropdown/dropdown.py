f67a4f31b07c306c05c9c71107f0d58b5651455c
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2022-10-11 14:08:35 +0600

    Dropdown

---
+++ 2.WorkingWithWebElements/6.Dropdown/dropdown.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+
+class Dropdown():
+    def dropdown(self):
+        driver = webdriver.Firefox(
+            executable_path="E:\\Ismail\\Projects\\Automation_ISM\\Drivers\\geckodriver.exe")
+
+        driver.get("https://the-internet.herokuapp.com/dropdown")
+        time.sleep(5)
+
+        dropdown_list = driver.find_element(By.ID, 'dropdown')
+        sel = Select(dropdown_list)
+        #sel.select_by_visible_text('Option 1')
+        #sel.select_by_value("2")
+        sel.select_by_index(1)
+        time.sleep(3)
+
+        driver.close()
+
+
+test_obj = Dropdown()
+test_obj.dropdown()
